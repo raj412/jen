3d8314087fad0d229ec9e9f3242345a0eb5369fd
raj412 <raj412214@gmail.com>
2018-03-20 12:14:07 +0530

    remove

--- wp-config.php
+++ wp-config.php
@@ -17,13 +17,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wpweb101_site');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'wpweb101');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wpweb101@2015');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
